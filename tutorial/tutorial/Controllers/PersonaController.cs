1b60c744b83f974f4d6de6126a48c4d41d4b3efd
Joaquin <monocarp77@gmail.com>
2019-07-23 17:12:40 -0300

    primera carga

---
+++ tutorial/tutorial/Controllers/PersonaController.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using tutorial.Models;
+
+namespace tutorial.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+
+            try
+            {
+                
+                using (BDtutorial bd = new BDtutorial()) 
+                {
+
+                    // De esta forma se mapean datos de distintas tablas usando consultas SQL
+                    string sql = @"
+                            select p.id as id, p.nombres, p.apellidos, p.sexo, p.edad, p.fechaRegistro, c.nombreCiudad as nombreCiudad
+                            from Persona p
+                            inner join Ciudad c on c.id = p.codCiudad"; // Termina la consulta SQL
+                    return View(bd.Database.SqlQuery<PersonaCE>(sql).ToList()); // Muestra los datos encontrados a traves de la consulta SQL 
+                    
+                    // De esta forma se mapean datos de distintas tablas, segun el modelo fijado en PersonaCE (ver clase PersonaCE)
+                    //var data = from p in bd.Persona
+                    //           join c in bd.Ciudad on p.codCiudad equals c.id
+                    //           select new PersonaCE()
+                    //           {
+
+                    //               id = p.id,
+                    //               nombres = p.nombres,
+                    //               apellidos = p.apellidos,
+                    //               sexo = p.sexo,
+                    //               edad = p.edad,
+                    //               nombreCiudad = c.nombreCiudad,
+                    //               fechaRegistro = p.fechaRegistro                                   
+
+                    //           };
+
+                    //List<Persona> listaPersonas= bd.Persona.Where(p=> p.edad>25).ToList(); // Muestra aquella personas que sean mayores de 25 años
+                    //return View(data.ToList()); // Se pasan los datos buscados anteriormente a la vista a traves de var data!
+
+
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public ActionResult crearPersona() // Metodo para crear una persona en la lista
+        {
+            return View();
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]                      // Valida que el formulario que se esta enviando sea el correcto
+        public ActionResult crearPersona(Persona p)
+        {
+
+            try
+            {
+
+                if (!ModelState.IsValid)
+                    return View();
+
+                using (BDtutorial bd=new BDtutorial())
+                {
+
+                    p.fechaRegistro = DateTime.Now;
+                    bd.Persona.Add(p);
+                    bd.SaveChanges(); // Guardo los datos de la nueva persona creada
+                    return RedirectToAction("Index");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError("", "Error al crear la persona " + ex.Message);
+                return View();
+
+            }
+                        
+        }
+
+        public ActionResult listarCiudades()
+        {
+
+            try
+            {
+
+                using (BDtutorial bd = new BDtutorial())
+                {
+
+                    return PartialView(bd.Ciudad.ToList());
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+        }
+
+        public ActionResult editarPersona(int id)
+        {
+            try
+            {
+                using (BDtutorial bd = new BDtutorial())
+                {
+                    //Persona pe = bd.Persona.Where(p => p.id == id).FirstOrDefault(); // Esta busqueda se puede utilizar siempre
+                    Persona per = bd.Persona.Find(id); // Esta busqueda solo se puede usar cuando hay una sola clave primaria en la tabla
+                    return View(per);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult editarPersona(Persona p)
+        {
+
+            try
+            {
+
+                if (!ModelState.IsValid)
+                    return View();
+
+                using (BDtutorial bd = new BDtutorial())
+                {
+                    Persona pe = bd.Persona.Find(p.id);
+
+                    pe.nombres = p.nombres;
+                    pe.apellidos = p.apellidos;
+                    pe.sexo = p.sexo;
+                    pe.edad = p.edad;
+
+                    bd.SaveChanges(); // Guardo los cambios realizados sobre los datos de la persona
+
+                    return RedirectToAction("Index");
+
+                }
+                    
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            
+        }
+
+        public ActionResult detallesPersona(int id)
+        {
+
+            try
+            {
+
+                using (BDtutorial bd = new BDtutorial())
+                {
+
+                    //Persona pe = bd.Persona.Where(p => p.id == id).FirstOrDefault(); // Esta busqueda se puede utilizar siempre
+                    Persona per = bd.Persona.Find(id); // Esta busqueda solo se puede usar cuando hay una sola clave primaria en la tabla
+                    return View(per);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+        }
+
+        public ActionResult eliminarPersona(int id)
+        {
+
+            try
+            {
+
+                using (BDtutorial bd = new BDtutorial())
+                {
+
+                    //Persona pe = bd.Persona.Where(p => p.id == id).FirstOrDefault(); // Esta busqueda se puede utilizar siempre
+                    Persona per = bd.Persona.Find(id); // Esta busqueda solo se puede usar cuando hay una sola clave primaria en la tabla
+                    bd.Persona.Remove(per); // Elimino la persona que encontre en la linea anterior
+
+                    bd.SaveChanges(); // Guardo los cambios
+
+                    return RedirectToAction("Index");
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public static string nombreCiudad(int codCiudad) // Metodo para obtener el nombre de la Ciudad partiendo desde Persona. 
+        {
+
+            try
+            {
+
+                using (BDtutorial bd = new BDtutorial())
+                {
+
+                    return bd.Ciudad.Find(codCiudad).nombreCiudad;
+
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ tutorial/tutorial/Models/PersonaCE.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace tutorial.Models
+{
+    public class PersonaCE
+    {
+        public int id { get; set; }
+        [Required]                              // Solicita si o si que se llene este campo
+        [Display(Name = "Ingrese Nombres")]      // Modifica el label del campo a llenar. Antes era "nombres"
+        public string nombres { get; set; }
+        [Required]
+        [Display(Name = "Ingrese Apellidos")]
+        public string apellidos { get; set; }
+        [Required]
+        [Display(Name = "Ingrese Sexo")]
+        public string sexo { get; set; }
+        [Required]
+        [Display(Name = "Ingrese Edad")]
+        public int edad { get; set; }
+        [Required]
+        [Display(Name = "Ciudad")]
+        public int codCiudad { get; set; }
+
+        public string nombreCiudad { get; set; }
+        public string nombreCompleto { get { return nombres + " " + apellidos; } } // metodo para mostrar el nombre y apellido juntos.
+        public System.DateTime fechaRegistro { get; set; }
+
+    }
+       
+    [MetadataType(typeof(PersonaCE))]
+
+    public partial class Persona
+    {
+        public string nombreCompleto { get { return nombres + " " + apellidos; } } // metodo para mostrar el nombre y apellido juntos.
+        public string nombreCiudad { get; set; } // Lo debo agregar para poder mapear correctamente
+    }
+    
+}
